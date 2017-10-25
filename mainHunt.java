b90f431a45f463d7c4b4d56693b4245506e79c37
Hayden N. Walters <31044184+hnwalters@users.noreply.github.com>
2017-10-25 14:25:29 -0500

    Does not run.
    Exception in thread "main" java.lang.NumberFormatException: For input string: "7	15"
    	at java.lang.NumberFormatException.forInputString(Unknown Source)
    	at java.lang.Integer.parseInt(Unknown Source)
    	at java.lang.Integer.valueOf(Unknown Source)
    	at mainHunt.main(mainHunt.java:27)

--- mainHunt.java
+++ mainHunt.java
@@ -6,12 +6,13 @@
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
-public class mainHunt {
+public class mainHunt 
+{
 
 	public static void main(String[] args) throws IOException
 	{
 		Scanner fin=new Scanner(new FileReader("rooms.txt"));
-		int n=fin.nextInt();
+		int n=Integer.valueOf(fin.nextLine());
 			rooms [] a=new rooms[n];
 			
 			for(int i=0;i<n;i++)
@@ -21,8 +22,15 @@ public class mainHunt {
 			for(int i=0;i<n;i++)
 			{
 				String[] b=fin.nextLine().split(" ");
+				String description=fin.nextLine();
+				int adj1=Integer.valueOf(b[1]);
+				int adj2=Integer.valueOf(b[2]);
+				int adj3=Integer.valueOf(b[3]);
+				rooms[] adjs= { a[adj1-1], a[adj2-1], a[adj3-1]};
+				a[i].setAdj(adjs);
+				a[i].setDesc(description);
 			}
-			
+				
 			
 			
 				fin.close();
@@ -56,6 +64,7 @@ public class mainHunt {
 			if(command.equalsIgnoreCase("Move"))
 			{
 				System.out.println("To which room?");
+				
 			}
 			if(command.equalsIgnoreCase("Quit"))
 			{
